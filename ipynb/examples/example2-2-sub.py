6aac96515c294a6928d8f499d57f8fbe03ae214d
Ryan J. O'Neil <ryanjoneil@gmail.com>
2015-09-14 00:07:14 -0400

    last iteration

--- ipynb/examples/example2-2-sub.py
+++ ipynb/examples/example2-2-sub.py
@@ -2,20 +2,26 @@ from mosek.fusion import Model, Domain, Expr, ObjectiveSense
 import sys
 
 # Example 2. Column generation approach.
-# Iteration 1, subproblem.
+# Iteration 2, subproblem.
 # Output:
 #
 #    Images:
-#        w_1 = 0
+#        w_1 = 1
 #        w_2 = 1
 #        w_3 = 1
 #
 #    Commands:
 #        y_a = 0
 #        y_b = 1
-#        y_c = 1
-#        y_d = 1
-
+#        y_c = 0
+#        y_d = 0
+#
+#    Interactions:
+#        m_2 = 0
+#        m_3 = 0
+#        n_b = 1
+#        n_c = 0
+#        n_d = 0
 
 r = {'A': 5.0, 'B': 10.0, 'C': 7.0, 'D': 12.0}
 
@@ -45,6 +51,24 @@ z_2_d = m.variable('z_2_d', *binary)
 z_3_b = m.variable('z_3_b', *binary)
 z_3_c = m.variable('z_3_c', *binary)
 z_3_d = m.variable('z_3_d', *binary)
+
+# Variables to take images or commands from another clique.
+m_2 = m.variable('m_2', *binary)
+m_3 = m.variable('m_3', *binary)
+
+n_b = m.variable('n_b', *binary)
+n_c = m.variable('n_c', *binary)
+n_d = m.variable('n_d', *binary)
+
+# If something is taken out of a clique, it must either be put in the new
+# clique or incur its own cost.
+q_2_b = m.variable('q_2_b', *binary)
+q_2_c = m.variable('q_2_c', *binary)
+q_2_d = m.variable('q_2_d', *binary)
+
+q_3_b = m.variable('q_3_b', *binary)
+q_3_c = m.variable('q_3_c', *binary)
+q_3_d = m.variable('q_3_d', *binary)
 
 # Inclusion of an image and a command means that image must
 # use all command invocation from the clique.
@@ -93,6 +117,23 @@ m.constraint('c_3_d_1', Expr.sub(z_3_d, w_3), Domain.lessThan(0.0))
 m.constraint('c_3_d_2', Expr.sub(z_3_d, y_d), Domain.lessThan(0.0))
 m.constraint('c_3_d_3', Expr.sub(z_3_d, Expr.add([w_3, y_d])), Domain.greaterThan(-1.0))
 
+# Taking something from an existing clique means we must incur it cost.
+m.constraint('d_2_b', Expr.sub(z_2_b, Expr.add([m_2, n_b])), Domain.lessThan(0.0))
+m.constraint('d_2_c', Expr.sub(z_2_c, Expr.add([m_2, n_c])), Domain.lessThan(0.0))
+m.constraint('d_2_d', Expr.sub(z_2_d, Expr.add([m_2, n_d])), Domain.lessThan(0.0))
+
+m.constraint('d_2_b_q_2_b', Expr.sub(Expr.add([z_2_b, q_2_b]), Expr.add([m_2, n_b])), Domain.greaterThan(0.0))
+m.constraint('d_2_c_q_2_c', Expr.sub(Expr.add([z_2_c, q_2_c]), Expr.add([m_2, n_c])), Domain.greaterThan(0.0))
+m.constraint('d_2_d_q_2_d', Expr.sub(Expr.add([z_2_d, q_2_d]), Expr.add([m_2, n_d])), Domain.greaterThan(0.0))
+
+m.constraint('d_3_b', Expr.sub(z_3_b, Expr.add([m_3, n_b])), Domain.lessThan(0.0))
+m.constraint('d_3_c', Expr.sub(z_3_c, Expr.add([m_3, n_c])), Domain.lessThan(0.0))
+m.constraint('d_3_d', Expr.sub(z_3_d, Expr.add([m_3, n_d])), Domain.lessThan(0.0))
+
+m.constraint('d_3_b_q_3_b', Expr.sub(Expr.add([z_3_b, q_3_b]), Expr.add([m_3, n_b])), Domain.greaterThan(0.0))
+m.constraint('d_3_c_q_3_c', Expr.sub(Expr.add([z_3_c, q_3_c]), Expr.add([m_3, n_c])), Domain.greaterThan(0.0))
+m.constraint('d_3_d_q_3_d', Expr.sub(Expr.add([z_3_d, q_3_d]), Expr.add([m_3, n_d])), Domain.greaterThan(0.0))
+
 # Maximize the amount we can improve our objective by adding a new clique.
 obj1 = [Expr.mul(c, y) for c, y in [
     (r['A'], y_a), (r['B'], y_b), (r['C'], y_c), (r['D'], y_d)
@@ -100,11 +141,22 @@ obj1 = [Expr.mul(c, y) for c, y in [
 obj2 = [Expr.mul(c, z) for c, z in [
     # Individual image/command pairs
     (r['A'], z_1_a), (r['B'], z_1_b),
-    (r['A'], z_2_a), (r['B'], z_2_b), (r['C'], z_2_c), (r['D'], z_2_d),
-    (r['B'], z_3_b), (r['C'], z_3_c), (r['D'], z_3_d),
+    (r['A'], z_2_a),
+]]
+obj3 = [Expr.mul(c, z) for c, z in [
+    # Individual image/command pairs for commands that are now run alone
+    (r['B'], q_2_b), (r['C'], q_2_c), (r['D'], q_2_d),
+    (r['B'], q_3_b), (r['C'], q_3_c), (r['D'], q_3_d),
+]]
+obj4 = [Expr.mul(c, y) for c, y in [
+    # Commands taken out of the existing cliques
+    (r['B'], n_b), (r['C'], n_c), (r['D'], n_d)
 ]]
 
-m.objective('w', ObjectiveSense.Maximize, Expr.sub(Expr.add(obj2), Expr.add(obj1)))
+
+m.objective('w', ObjectiveSense.Maximize,
+    Expr.sub(Expr.add(obj2 + obj4), Expr.add(obj1 + obj3))
+)
 m.setLogHandler(sys.stdout)
 m.solve()
 
@@ -120,4 +172,12 @@ print '\ty_a = %.0f' % y_a.level()[0]
 print '\ty_b = %.0f' % y_b.level()[0]
 print '\ty_c = %.0f' % y_c.level()[0]
 print '\ty_d = %.0f' % y_d.level()[0]
+print
+
+print 'Interactions:'
+print '\tm_2 = %.0f' % m_2.level()[0]
+print '\tm_3 = %.0f' % m_3.level()[0]
+print '\tn_b = %.0f' % n_b.level()[0]
+print '\tn_c = %.0f' % n_c.level()[0]
+print '\tn_d = %.0f' % n_d.level()[0]
 print
--- ipynb/examples/example2-3-master.py
+++ ipynb/examples/example2-3-master.py
@@ -2,7 +2,7 @@ from mosek.fusion import Model, Domain, Expr, ObjectiveSense
 import sys
 
 # Example 2. Column generation approach.
-# Iteration 2, master problem.
+# Iteration 3, master problem.
 # Output:
 #
 #    Image 1:
@@ -45,16 +45,25 @@ x_3_d = m.variable('x_3_d', *binary)
 # Provide a variable for each maximal clique and maximal sub-clique.
 x_23_bcd = m.variable('x_23_bcd', *binary)
 
+x_123_b = m.variable('x_123_b', *binary)
+x_123_b_23_cd = m.variable('x_123_b_23_cd', *binary)
+
 # Each command must be run once for each image.
 m.constraint('c_1_a', Expr.add([x_1_a]), Domain.equalsTo(1.0))
-m.constraint('c_1_b', Expr.add([x_1_b]), Domain.equalsTo(1.0))
+m.constraint('c_1_b', Expr.add([x_1_b, x_123_b]), Domain.equalsTo(1.0))
 m.constraint('c_2_a', Expr.add([x_2_a]), Domain.equalsTo(1.0))
-m.constraint('c_2_b', Expr.add([x_2_b, x_23_bcd]), Domain.equalsTo(1.0))
-m.constraint('c_2_c', Expr.add([x_2_c, x_23_bcd]), Domain.equalsTo(1.0))
-m.constraint('c_2_d', Expr.add([x_2_d, x_23_bcd]), Domain.equalsTo(1.0))
-m.constraint('c_3_b', Expr.add([x_3_b, x_23_bcd]), Domain.equalsTo(1.0))
-m.constraint('c_3_c', Expr.add([x_3_c, x_23_bcd]), Domain.equalsTo(1.0))
-m.constraint('c_3_d', Expr.add([x_3_d, x_23_bcd]), Domain.equalsTo(1.0))
+m.constraint('c_2_b', Expr.add([x_2_b, x_23_bcd, x_123_b]), Domain.equalsTo(1.0))
+m.constraint('c_2_c', Expr.add([x_2_c, x_23_bcd, x_123_b_23_cd]), Domain.equalsTo(1.0))
+m.constraint('c_2_d', Expr.add([x_2_d, x_23_bcd, x_123_b_23_cd]), Domain.equalsTo(1.0))
+m.constraint('c_3_b', Expr.add([x_3_b, x_23_bcd, x_123_b]), Domain.equalsTo(1.0))
+m.constraint('c_3_c', Expr.add([x_3_c, x_23_bcd, x_123_b_23_cd]), Domain.equalsTo(1.0))
+m.constraint('c_3_d', Expr.add([x_3_d, x_23_bcd, x_123_b_23_cd]), Domain.equalsTo(1.0))
+
+# Add dependency constraints for sub-cliques.
+m.constraint('d_123_b_23_cd', Expr.sub(x_123_b, x_123_b_23_cd), Domain.greaterThan(0.0))
+
+# Eliminated intersections between cliques.
+m.constraint('e1', Expr.add([x_23_bcd, x_123_b]), Domain.lessThan(1.0))
 
 # Minimize resources required to construct all images.
 obj = [Expr.mul(c, x) for c, x in [
@@ -65,6 +74,8 @@ obj = [Expr.mul(c, x) for c, x in [
 
     # Cliques
     (r['B'] + r['C'] + r['D'], x_23_bcd),
+    (r['B'], x_123_b),
+    (r['C'] + r['D'], x_123_b_23_cd),
 ]]
 m.objective('w', ObjectiveSense.Minimize, Expr.add(obj))
 m.setLogHandler(sys.stdout)
@@ -91,4 +102,6 @@ print
 
 print 'Cliques:'
 print '\tx_23_bcd = %.0f' % x_23_bcd.level()[0]
+print '\tx_123_b = %.0f' % x_123_b.level()[0]
+print '\tx_123_b_23_cd = %.0f' % x_123_b_23_cd.level()[0]
 print
