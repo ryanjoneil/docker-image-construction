8a6225799c19f156c4b8ca7ef8672ff3c68d61ab
Ryan J. O'Neil <ryanjoneil@gmail.com>
2016-01-02 16:43:29 -0500

    adding gurobi-based clique model

--- dicp/solvers/__init__.py
+++ dicp/solvers/__init__.py
@@ -1,14 +1,15 @@
 from .benders_model_gurobi import BendersModelGurobi
 from .bip_model_gurobi import BIPModelGurobi
 from .bip_model_mosek import BIPModelMosek
+from .clique_model_gurobi import CliqueModelGurobi
 from .clique_model_mosek import CliqueModelMosek
 from .most_common import MostCommonHeuristic
 from .most_time import MostTimeHeuristic
 from .network_mosek import NetworkMosek
 
 ALL_SOLVERS = (
-    BendersModelGurobi, BIPModelGurobi, BIPModelMosek, CliqueModelMosek,
-    MostCommonHeuristic, MostTimeHeuristic, NetworkMosek
+    BendersModelGurobi, BIPModelGurobi, BIPModelMosek, CliqueModelGurobi,
+    CliqueModelMosek, MostCommonHeuristic, MostTimeHeuristic, NetworkMosek
 )
 
 __all__ = 'ALL_SOLVERS',
---
+++ dicp/solvers/clique_model_gurobi.py
@@ -0,0 +1,93 @@
+from collections import defaultdict
+from itertools import product
+from gurobipy import GRB, Model, quicksum as sum
+import sys
+
+class CliqueModelGurobi(object):
+    '''Transformed set packing model over maximal cliques'''
+    _slug = 'clique-model-gurobi'
+
+    def __init__(self, time=None):
+        self.time = time # in minutes
+
+    def slug(self):
+        return CliqueModelGurobi._slug
+
+    def solve(self, problem, saver):
+        # Construct model.
+        self.problem = problem
+
+        # Do recursive maximal clique detection.
+        self.clique_data = clique_data = problem.cliques()
+        self.model = model = Model()
+        if self.time is not None:
+            model.params.TimeLimit = 60 * int(self.time)
+
+        # Each image needs to run all its commands. This keep track of
+        # what variables run each command for each image.
+        self.by_img_cmd = by_img_cmd = defaultdict(list)
+
+        # Objective is the total cost of all the commands we run.
+        self._obj = []
+
+        # x[i,c] = 1 if image i incurs the cost of command c directly
+        self.x = x = {}
+        for img, cmds in problem.images.items():
+            for cmd in cmds:
+                name = 'x[%s,%s]' % (img, cmd)
+                x[name] = v = model.addVar(vtype=GRB.BINARY, name=name)
+                self._obj.append(problem.commands[cmd] * v)
+                by_img_cmd[img,cmd].append(v)
+
+        # cliques[i] = 1 if clique i is used, 0 otherwise
+        self.cliques = {}
+        self._update(clique_data)
+
+        # Each image has to run each of its commands.
+        for img_cmd, vlist in by_img_cmd.items():
+            self.model.addConstr(sum(vlist) == 1)
+
+        model.setObjective(sum(self._obj), GRB.MINIMIZE)
+        model.optimize()#lambda *args: self._callback(saver, *args))
+
+        # TODO: callback
+        # Translate the output of this to a schedule.
+        schedule = defaultdict(list)
+        self._translate(schedule, clique_data)
+        for name, v in x.items():
+            img, cmd = name.replace('x[','').replace(']','').split(',')
+            if v.x > 0.5:
+                schedule[img].append(cmd)
+        saver(schedule)
+
+
+    def _update(self, clique_data, parent=None):
+        for c in clique_data['cliques']:
+            self.cliques[c['name']] = v = self.model.addVar(
+                vtype=GRB.BINARY,
+                name=c['name']
+            )
+            self.model.update()
+
+            for img, cmd in product(c['images'], c['commands']):
+                self.by_img_cmd[img,cmd].append(v)
+
+            self._obj.append(c['time'] * v)
+
+            if parent is not None:
+                self.model.addConstr(v <= self.cliques[parent['name']])
+
+            for child in c['children']:
+                self._update(child, c)
+
+        for inter in clique_data['intersections']:
+            self.model.addConstr(sum(self.cliques[i] for i in inter) <= 1)
+
+    def _translate(self, schedule, data):
+        for c in data['cliques']:
+            if self.cliques[c['name']].x > 0.5:
+                for img, cmd in product(c['images'], c['commands']):
+                    schedule[img].append(cmd)
+                for child in c['children']:
+                   self._translate(schedule, child)
+
