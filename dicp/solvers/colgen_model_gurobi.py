250ba0add2ce0a1ae9bd808605a39ce599bb74d6
Ryan J. O'Neil <ryanjoneil@gmail.com>
2016-04-12 00:05:15 -0400

    local version of subproblem

--- dicp/solvers/colgen_model_gurobi.py
+++ dicp/solvers/colgen_model_gurobi.py
@@ -44,7 +44,8 @@ class ColgenModelGurobi(object):
 
             done = True
             self._master()
-            for clique in self._subproblem():
+            # for clique in self._subproblem_global():
+            for clique in self._subproblem_local():
                 if clique is not None and clique not in self.cliques:
                     print '[new clique] %s' % clique
 
@@ -143,12 +144,11 @@ class ColgenModelGurobi(object):
                     print '[clique/inter dual] %s | %s = %.02f' % (c1, c2, c.pi)
                 self.clique_inter_duals[c1, c2] = c.pi
 
-    def _subproblem(self):
+    def _subproblem_global(self):
         cliques = []
 
         model = Model()
         # model.params.OutputFlag = False
-        model.params.LazyConstraints = True
         obj = []
 
         y = {}  # Do we resolve the intersection?
@@ -239,5 +239,105 @@ class ColgenModelGurobi(object):
 
             if len(rem_imgs) > 1:
                 cliques.append(Clique(self.problem, rem_imgs, c.commands))
+
+        return cliques
+
+    def _subproblem_local(self):
+        cliques = []
+
+        for (c1, c2), pi in self.clique_inter_duals.items():
+            int_images = c1.images_set.intersection(c2.images_set)
+
+            # Remove images in c1 not in c2
+            z = -c1.cost
+            for i in int_images:
+                dual = sum(self.img_cmd_duals[i, cmd] for cmd in c1.commands)
+                z += dual
+
+            if z > 0:
+                cliques.append(Clique(self.problem, int_images, c1.commands))
+
+            # Remove images in c2 not in c1
+            z = -c2.cost
+            for i in int_images:
+                dual = sum(self.img_cmd_duals[i, cmd] for cmd in c1.commands)
+                z += dual
+
+            if z > 0:
+                cliques.append(Clique(self.problem, int_images, c2.commands))
+
+            # Pare images off until they don't intersect anymore
+            model = Model()
+            model.params.OutputFlag = False
+
+            # model.params.OutputFlag = False
+            obj = [pi]
+
+            p1 = model.addVar(vtype=GRB.BINARY)
+            p2 = model.addVar(vtype=GRB.BINARY)
+            q1 = {i: model.addVar(vtype=GRB.BINARY) for i in c1.images}
+            q2 = {i: model.addVar(vtype=GRB.BINARY) for i in c2.images}
+            r1 = {i: model.addVar(vtype=GRB.BINARY) for i in int_images}
+            r2 = {i: model.addVar(vtype=GRB.BINARY) for i in int_images}
+
+            model.update()
+
+            obj.append(-c1.cost * p1)
+            obj.append(-c2.cost * p2)
+
+            for i in c1.images:
+                dual = sum(self.img_cmd_duals[i, cmd] for cmd in c1.commands)
+                obj.append(dual * q1[i])
+
+            for i in c2.images:
+                dual = sum(self.img_cmd_duals[i, cmd] for cmd in c2.commands)
+                obj.append(dual * q2[i])
+
+            for i in int_images:
+                model.addConstr(p1 <= r1[i] + r2[i])
+                model.addConstr(p2 <= r1[i] + r2[i])
+
+            for i in c1.images:
+                model.addConstr(q1[i] <= p1)
+                if i in int_images:
+                    model.addConstr(q1[i] <= 1 - r1[i])
+
+            for i in c2.images:
+                model.addConstr(q2[i] <= p1)
+                if i in int_images:
+                    model.addConstr(q2[i] <= 1 - r2[i])
+
+            model.setObjective(sum(obj), GRB.MAXIMIZE)
+            model.optimize()
+
+            for c, v, r in [(c1, p1, r1), (c2, p2, r2)]:
+                if v.x < 0.5:
+                    continue
+
+                # Figure out what images are left in the clique
+                rem_imgs = set(c.images)
+                for i, riv in r.items():
+                    if riv.x > 0.5:
+                        rem_imgs.remove(i)
+
+                if len(rem_imgs) > 1:
+                    cliques.append(Clique(self.problem, rem_imgs, c.commands))
+                rem_imgs_1 = set(c1.images)
+                rem_imgs_2 = set(c2.images)
+
+                z = -c1.cost - c2.cost
+                for i in int_images:
+                    dual1 = sum(self.img_cmd_duals[i, cmd] for cmd in c1.commands)
+                    dual2 = sum(self.img_cmd_duals[i, cmd] for cmd in c2.commands)
+                    if dual1 > dual2:
+                        rem_imgs_1.remove(i)
+                        z += dual1
+                    else:
+                        rem_imgs_2.remove(i)
+                        z += dual2
+
+                if z > 0:
+                    cliques.append(Clique(self.problem, rem_imgs_1, c1.commands))
+                    cliques.append(Clique(self.problem, rem_imgs_2, c2.commands))
 
         return cliques
